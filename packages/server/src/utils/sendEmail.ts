0f31d33924c56435a951832498ed5b9ac85ac0ea
Aleksandr Schemelev <prophreaker@gmail.com>
2021-01-14 17:30:47 +0300

    sparkpost domain change

--- packages/server/src/utils/sendEmail.ts
+++ packages/server/src/utils/sendEmail.ts
@@ -1,16 +1,16 @@
 import SparkPost from "sparkpost";
 
 const client = new SparkPost(process.env.SPARKPOST_API_KEY, {
-  endpoint: "https://api.eu.sparkpost.com:443"
+  endpoint: 'https://api.sparkpost.com:443'
 });
 
 export const sendEmail = async (recipient: string, url: string) => {
   const response = await client.transmissions.send({
     options: {
-      sandbox: process.env.NODE_ENV === 'test' ? true : false,
+      sandbox: false,
     },
     content: {
-      from: "testing@sparkpostbox.com",
+      from: "register@zero-studio.art",
       subject: "Confirm Email!",
       html: `<html><body><a href="${url}" target="_blank">Confirm your email</a></body></html>`,
     },
